bbba9d0e5e9fc9d52eb0590c587fb73e9eedc640
Fabio Matos <fabio.thunder@hotmail.com>
2021-01-13 14:37:15 -0300

    aprendendo beneficios da herança

---
+++ bytebank-herdado/src/TesteGerente.java
@@ -0,0 +1,26 @@
+
+public class TesteGerente {
+
+	public static void main(String[] args) {
+		
+		Gerente g1 = new Gerente();
+		g1.setName("Marco Aurélio");
+		g1.setCpf("23456787");
+		g1.setSalario(5000.0);
+		
+		System.out.println(g1.getName());
+		System.out.println(g1.getCpf());
+		System.out.println(g1.getSalario());
+		
+		
+		
+		g1.setSenha(222);
+		boolean autenticou = g1.autentica(222);
+		
+		System.out.println(autenticou);
+		
+		
+
+	}
+
+}
