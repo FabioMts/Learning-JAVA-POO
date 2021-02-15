4b3da6e287ce2c8c0e897ed928ccecc6f7e34591
Fabio Matos <fabio.thunder@hotmail.com>
2021-02-14 22:54:51 -0300

    comands break and continue

--- curso-java-basico/src/com/loiane/cursojava/aula17/labs/Exercicios.java
+++ curso-java-basico/src/com/loiane/cursojava/aula17/labs/Exercicios.java
@@ -161,90 +161,218 @@ public class Exercicios {
 		
 		// ex 5
 		
-		double popA;
-		double popB;
-		double taxaA;
-		double taxaB;
+//		double popA;
+//		double popB;
+//		double taxaA;
+//		double taxaB;
+//		
+//		boolean valido = false;
+//		
+//		do {
+//			
+//			System.out.println("Entre com a população A: ");
+//			popA = scan.nextDouble();
+//			
+//			if(popA > 0) {
+//				valido = true;
+//			}else {
+//				System.out.println("População A precisa ser maior que 0.");
+//			}
+//		}while (!valido);
+//
+//		 valido = false;
+//		
+//		do {
+//			
+//			System.out.println("Entre com a população B: ");
+//			popB = scan.nextDouble();
+//			
+//			if(popB > 0) {
+//				valido = true;
+//			}else {
+//				System.out.println("População B precisa ser maior que 0.");
+//			}
+//		}while (!valido);
+//		
+//		
+//		 valido = false;
+//			
+//			do {
+//				
+//				System.out.println("Entre com a taxa de crescimento da população A: ");
+//				taxaA = scan.nextDouble();
+//				
+//				if(taxaA > 0) {
+//					valido = true;
+//				}else {
+//					System.out.println("A taxa de crescimento da população A precisa ser maior do que 0.");
+//				}
+//			}while (!valido);
+//			
+//			valido = false;
+//			
+//			do {
+//				
+//				System.out.println("Entre com a taxa de crescimento da população B: ");
+//				taxaB = scan.nextDouble();
+//				
+//				if(taxaB > 0) {
+//					valido = true;
+//				}else {
+//					System.out.println("A taxa de crescimento da população B precisa ser maior do que 0.");
+//				}
+//			}while (!valido);
+//		
+//			
+//			int cont = 0;
+//			
+//			while (popA < popB) {
+//				
+//				popA += (popA/100) * taxaA;
+//				popB += (popB/100) * taxaB;
+//				cont++;
+//			}
+//			
+//			System.out.println("População A: " + popA);
+//			System.out.println("População B: " + popB);
+//			System.out.println("Quantidade de anos: " + cont);
+//				
+//			
+			//ex 6
 		
-		boolean valido = false;
+//			for(int i =0; i <=20; i++) {
+//				System.out.print(i + " ");
+//			}
+//			
 		
-		do {
-			
-			System.out.println("Entre com a população A: ");
-			popA = scan.nextDouble();
-			
-			if(popA > 0) {
-				valido = true;
-			}else {
-				System.out.println("População A precisa ser maior que 0.");
-			}
-		}while (!valido);
+			//ex 7
+		
+		
+//		int num;
+//		int maior = Integer.MIN_VALUE;
+//			for(int i =0; i<5; i++) {
+//				System.out.println("Entre com um número: ");			
+//				num = scan.nextInt();
+//				
+//				if(num > maior) {
+//					maior = num;
+//				}
+//				
+//				
+//			}
+//				
+//				System.out.println("O maior número digitado foi: " + maior);
+		
+		
+		
+		
+		
+			//ex 8
+//		
+//		int num;
+//		int soma=0;
+//		double media;
+//		
+//			for(int i =0; i<5; i++) {
+//				System.out.println("Entre com um número: ");			
+//				num = scan.nextInt();
+//		
+//				soma += num;
+//				
+//			}
+//			media = soma/5;
+//			System.out.println("Soma: " + soma + " | Media: " + media);
+		
+		
+		
+		//ex 9
+		
+//			for(int i=0; i < 50; i++) { 
+//		
+//			if(i % 2 != 0) {
+//				System.out.println(i);
+//			}
+//		}
+//		
+		
+		//ex 10
+		
+//		System.out.println("Entre com o primeiro numero: ");
+//		int num1 = scan.nextInt();
+//		System.out.println("Entre com o segundo numero: ");
+//		int num2 = scan.nextInt();
+//		
+//		for(int i=num1; i<= num2; i++) {
+//			System.out.println(i);
+//		}
+		
+		
+		//ex 11
+		
+//		double soma = 0;
+//		System.out.println("Entre com o primeiro numero: ");
+//		int num1 = scan.nextInt();
+//		System.out.println("Entre com o segundo numero: ");
+//		int num2 = scan.nextInt();
+//		
+//		for(int i=num1; i<= num2; i++) {
+//			soma += i;
+//		}
+//		System.out.println("soma: " + soma);
 
-		 valido = false;
 		
-		do {
+		
+		//ex 12
+//			int num;
+//			
+//			System.out.println("Digite o número que deseja ver a tabuada: ");
+//			num = scan.nextInt();
+//			
+//			System.out.println("Tabuada de " + num + ": ");
+//			
+//			for(int i=0; i <=10; i++) {
+//				System.out.println(num + " x " + i + " = " + num * i);
+//			}
 			
-			System.out.println("Entre com a população B: ");
-			popB = scan.nextDouble();
+		// ex 13
+		
+//			System.out.println("Entre com a base: ");
+//			int base = scan.nextInt();
+//			System.out.println("Entre com a potência: ");
+//			int pot = scan.nextInt();
+//			
+//			int resultado = base;
+//			
+//			for(int i=1; i<pot; i++) {
+//				resultado *= base;
+//			}
+//		System.out.println("Resultado: " + resultado);
+		
+		
+		
+		//ex 14
+		
+		
+		int num;
+		int pares = 0;
+		int impares = 0;
+		
+		for(int i =0; i <= 10; i++) {
+			System.out.println("Entre com um número");
+			num = scan.nextInt();
 			
-			if(popB > 0) {
-				valido = true;
+			if (num % 2 ==0) {
+				pares++;
 			}else {
-				System.out.println("População B precisa ser maior que 0.");
+				impares++;
 			}
-		}while (!valido);
+		}
+			System.out.println("Pares: " + pares);
+			System.out.println("impares: " + impares);
 		
 		
-		 valido = false;
-			
-			do {
-				
-				System.out.println("Entre com a taxa de crescimento da população A: ");
-				taxaA = scan.nextDouble();
-				
-				if(taxaA > 0) {
-					valido = true;
-				}else {
-					System.out.println("A taxa de crescimento da população A precisa ser maior do que 0.");
-				}
-			}while (!valido);
-			
-			valido = false;
-			
-			do {
-				
-				System.out.println("Entre com a taxa de crescimento da população B: ");
-				taxaB = scan.nextDouble();
-				
-				if(taxaB > 0) {
-					valido = true;
-				}else {
-					System.out.println("A taxa de crescimento da população B precisa ser maior do que 0.");
-				}
-			}while (!valido);
 		
-			
-			int cont = 0;
-			
-			while (popA < popB) {
-				
-				popA += (popA/100) * taxaA;
-				popB += (popB/100) * taxaB;
-				cont++;
-			}
-			
-			System.out.println("População A: " + popA);
-			System.out.println("População B: " + popB);
-			System.out.println("Quantidade de anos: " + cont);
-				
-			
-			
-			
-			
-			
-			
-			
-			}
+		}
 	}
 
 
