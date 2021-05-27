670ceee4628c0700de9f0a9629ee4b5eed36d2aa
Fabio Matos <fabio.thunder@hotmail.com>
2021-05-26 23:01:15 -0300

    learning about instaceof, upcasting and downcasting

---
+++ curso-java-basico-loiane/src/com/loiane/cursojava/aula45/Teste.java
@@ -0,0 +1,34 @@
+package com.loiane.cursojava.aula45;
+
+public class Teste {
+	
+	
+	public static void main(String[] args) {
+		
+		/*Aluno aluno = new Aluno();
+		Pessoa pessoaAluno = aluno; //upcasting
+		
+		Pessoa aluno2 = (Pessoa) new Aluno();
+		
+		Pessoa aluno3 = new Pessoa();
+		Aluno aluno4 = (Aluno) aluno3;*/
+		
+		
+		Pessoa pessoa = new Pessoa();
+		Aluno aluno = new Aluno();
+		Professor prof = new Professor();
+		
+		if(pessoa instanceof Pessoa) {
+			System.out.println("e do tipo Pessoa");
+		}
+		
+		if(aluno instanceof Aluno) {
+			System.out.println("e do tipo Aluno");
+		}
+		
+		if(prof instanceof Professor) {
+			System.out.println("e do tipo Professor");
+		}
+	}
+
+}
