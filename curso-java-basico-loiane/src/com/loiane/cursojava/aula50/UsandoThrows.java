2e09ee9c3a175a14eaf48d7e42427bcbd95d1bba
Fabio Matos <fabio.thunder@hotmail.com>
2021-06-02 00:11:45 -0300

    learning more about class Throwable

---
+++ curso-java-basico-loiane/src/com/loiane/cursojava/aula50/UsandoThrows.java
@@ -0,0 +1,29 @@
+package com.loiane.cursojava.aula50;
+
+import java.util.Scanner;
+
+public class UsandoThrows {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Entre com um número decimal");
+		try {
+			double num = leNumero();
+			System.out.println("Voce digitou: " + num);
+		} catch (Exception e) {
+			System.out.println("Entrada inválida");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static double leNumero() throws Exception{
+		Scanner scan = new Scanner(System.in);
+		
+		double num = scan.nextDouble();
+		
+		return num;
+	}
+	
+
+}
