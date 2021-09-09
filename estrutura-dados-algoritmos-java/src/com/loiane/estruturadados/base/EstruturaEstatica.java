e0b83a8c28bbf4b868fa126df6aacf25a00c34e6
Fabio Matos <fabio.thunder@hotmail.com>
2021-09-08 22:38:00 -0300

    aprendendo mais sobre pilhas

--- estrutura-dados-algoritmos-java/src/com/loiane/estruturadados/base/EstruturaEstatica.java
+++ estrutura-dados-algoritmos-java/src/com/loiane/estruturadados/base/EstruturaEstatica.java
@@ -2,4 +2,79 @@ package com.loiane.estruturadados.base;
 
 public class EstruturaEstatica<T> {
 
+
+	private T[] elementos;
+	private int tamanho;
+
+	public EstruturaEstatica(int capacidade) {
+		this.elementos = (T[])new Object[capacidade];
+		this.tamanho = 0;
+	}
+	
+	public EstruturaEstatica() {
+		this(10);
+	}
+	
+	public boolean adiciona(T elemento) {
+		this.aumentaCapacidade();
+		if (this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+			return true;
+		}
+		return false;
+
+	}
+	
+	public boolean adiciona(int posicao, T elemento) {
+
+		if (!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Posicão invalida");
+		}
+
+		this.aumentaCapacidade();
+
+		// mover todos os elementos
+		for (int i = this.tamanho - 1; i >= posicao; i--) {
+			this.elementos[i + 1] = this.elementos[i];
+		}
+		this.elementos[posicao] = elemento;
+		this.tamanho++;
+
+		return true;
+	}
+	
+	private void aumentaCapacidade() {
+		if (this.tamanho == this.elementos.length) {
+			T[] elementosNovos = (T[])new Object[this.elementos.length * 2];
+			for (int i = 0; i < this.elementos.length; i++) {
+				elementosNovos[i] = this.elementos[i];
+			}
+			this.elementos = elementosNovos;
+		}
+	}
+	
+	public int tamanho() {
+		return this.tamanho;
+	}
+
+	@Override
+	public String toString() {
+
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+
+		for (int i = 0; i < this.tamanho - 1; i++) {
+			s.append(this.elementos[i]);
+			s.append(", ");
+		}
+
+		if (this.tamanho > 0) {
+			s.append(this.elementos[this.tamanho - 1]);
+		}
+		s.append("]");
+
+		return s.toString();
+	}
+	
 }
