699e8da4d4abf126ee6056ecac9cfe718ee489f9
Fabio Matos <fabio.thunder@hotmail.com>
2022-02-09 17:47:39 -0300

    data primitive types

--- relembrando-conceitos-Java/CharAndBoolean/Main.java
+++ relembrando-conceitos-Java/CharAndBoolean/Main.java
@@ -2,7 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
 
 
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
     }
 }
