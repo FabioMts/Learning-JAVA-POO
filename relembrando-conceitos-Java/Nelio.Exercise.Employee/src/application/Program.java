497b5ec53886837a37cd21ba1ee14f67f76905e9
Fabio Matos <fabio.thunder@hotmail.com>
2022-03-15 15:51:29 -0300

    more exercises about classes in java

---
+++ relembrando-conceitos-Java/Nelio.Exercise.Employee/src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import entities.Employee;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Employee emp = new Employee();
+
+
+        System.out.print("Name: ");
+        emp.name = sc.nextLine();
+
+        System.out.print("Gross Salary: ");
+        emp.grossSalary = sc.nextDouble();
+
+        System.out.print("Tax: ");
+        emp.tax = sc.nextDouble();
+
+        System.out.println(emp);
+
+        System.out.println();
+
+        System.out.print("Which percentage to increase salary? ");
+        double percentage = sc.nextDouble();
+        emp.increaseSalary(percentage);
+        System.out.println();
+        System.out.println("Update data: " + emp);
+
+
+
+
+
+        sc.close();
+
+    }
+}
