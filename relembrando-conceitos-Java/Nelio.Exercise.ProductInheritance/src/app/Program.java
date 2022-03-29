08a481c09dd4f9c0c16685ac799e59011881aae3
Fabio Matos <fabio.thunder@hotmail.com>
2022-03-29 15:43:07 -0300

    exercicio de fixação sobre heranca

---
+++ relembrando-conceitos-Java/Nelio.Exercise.ProductInheritance/src/app/Program.java
@@ -0,0 +1,66 @@
+package app;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Program {
+
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        List<Product> list = new ArrayList<>();
+
+        System.out.print("Enter the number of products: ");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++) {
+            System.out.println("Product #" + i + " data: ");
+            System.out.print("Common, used or imported (c/u/i)? ");
+            char ch = sc.next().charAt(0);
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Price: ");
+            double price = sc.nextDouble();
+
+
+            if(ch == 'i') {
+                System.out.print("Customs fee: ");
+                double customsFee = sc.nextDouble();
+                list.add(new ImportedProduct(name, price, customsFee));
+            }
+            else if(ch == 'u') {
+                System.out.print("Manufacture date: (DD/MM/YYYY): ");
+                Date manufactureDate = sdf.parse(sc.next());
+                list.add(new UsedProduct(name, price, manufactureDate));
+            }
+            else {
+                list.add(new Product(name, price));
+            }
+        }
+
+        System.out.println();
+        System.out.println("PRICE TAGS: ");
+
+        for(Product prod : list) {
+            System.out.println(prod.priceTag());
+        }
+
+
+        sc.close();
+
+
+    }
+
+
+}
