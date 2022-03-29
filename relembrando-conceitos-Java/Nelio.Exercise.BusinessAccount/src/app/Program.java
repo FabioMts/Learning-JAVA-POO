dcfa1a8fc6c0b6c303e58f8ce1cf4c4e8a52c377
Fabio Matos <fabio.thunder@hotmail.com>
2022-03-29 16:39:12 -0300

    classes abstratas

--- relembrando-conceitos-Java/Nelio.Exercise.BusinessAccount/src/app/Program.java
+++ relembrando-conceitos-Java/Nelio.Exercise.BusinessAccount/src/app/Program.java
@@ -1,21 +1,37 @@
 package app;
 
 import entities.Account;
+import entities.BusinessAccount;
 import entities.SavingsAccount;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
 
     public static void main(String[] args) {
 
+        List<Account> list = new ArrayList<>();
 
-        Account x = new Account(1020, "Alex", 1000.0);
-        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+        list.add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+        list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+        list.add(new SavingsAccount(1004, "Bob", 300.0, 0.01));
+        list.add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
 
-        x.withdraw(50.0);
-        y.withdraw(50.0);
+        double sum = 0.0;
+        for(Account acc : list) {
+            sum += acc.getBalance();
+        }
+        System.out.println("TOTAL BALANCE: " + sum);
 
-        System.out.println(x.getBalance());
-        System.out.println(y.getBalance());
+        for(Account acc : list) {
+            acc.deposit(10);
+        }
+
+        for(Account acc : list) {
+            System.out.printf("UPDATED BALANCE FOR ACCOUNT %d: %.2f%n", acc.getNumber(), acc.getBalance());
+        }
+
 
     }
 
