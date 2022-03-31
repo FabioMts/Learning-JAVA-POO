40dd32f5f922bdf62cfceb91965ce6495de080f6
Fabio Matos <fabio.thunder@hotmail.com>
2022-03-31 17:13:16 -0300

    usando default methods com interface

---
+++ relembrando-conceitos-Java/Nelio.Exemple.Emprestimo/src/services/UsaInterestService.java
@@ -0,0 +1,18 @@
+package services;
+
+public class UsaInterestService implements InterestService{
+
+    private double interestRate;
+
+    public UsaInterestService(double interestRate) {
+        this.interestRate = interestRate;
+    }
+    @Override
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+}
