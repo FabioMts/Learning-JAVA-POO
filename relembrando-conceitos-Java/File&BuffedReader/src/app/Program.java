53aa53eadd17c9788b85c7d0f487d66a8f72a325
Fabio Matos <fabio.thunder@hotmail.com>
2022-03-30 23:29:42 -0300

    jeito menos verboso de ler arquivos com File e BuffeReader

--- relembrando-conceitos-Java/File&BuffedReader/src/app/Program.java
+++ relembrando-conceitos-Java/File&BuffedReader/src/app/Program.java
@@ -10,12 +10,9 @@ public class Program {
 
         String path = "c\\temp\\in.txt";
 
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
 
             String line = br.readLine();
 
@@ -27,18 +24,6 @@ public class Program {
         }
         catch(IOException e) {
             System.out.println("Error: " + e.getMessage());
-        }
-        finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
 
     }
