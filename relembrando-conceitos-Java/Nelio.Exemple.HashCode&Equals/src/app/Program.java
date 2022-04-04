ae830b6f79f831cd8ad580ded20b9fc18f14c445
Fabio Matos <fabio.thunder@hotmail.com>
2022-04-04 12:05:49 -0300

    hashcode e equals

---
+++ relembrando-conceitos-Java/Nelio.Exemple.HashCode&Equals/src/app/Program.java
@@ -0,0 +1,26 @@
+package app;
+
+import entities.Client;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Client c1 = new Client("Maria", "Maria.com");
+        Client c2 = new Client("Maria", "Maria.com");
+
+        String s1 = "Test";
+        String s2 = "Test";
+
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        System.out.println(c1.equals(c2));
+        System.out.println(c1 == c2);
+        System.out.println(s1 == s2);
+
+
+
+
+    }
+
+}
