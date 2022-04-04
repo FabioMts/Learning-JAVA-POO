efb0a8007f814ab4bda24d083f5bd31976736256
Fabio Matos <fabio.thunder@hotmail.com>
2022-04-04 11:29:42 -0300

    tipo coringa

---
+++ relembrando-conceitos-Java/Nelio.Exemple.ShapeCoringa/src/entities/Rectangle.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class Rectangle implements Shape{
+
+    private double widht;
+    private double height;
+
+    public Rectangle(double widht, double height) {
+        super();
+        this.widht = widht;
+        this.height = height;
+    }
+
+    public double getWidht() {
+        return widht;
+    }
+
+    public void setWidht(double widht) {
+        this.widht = widht;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return widht * height;
+    }
+}
