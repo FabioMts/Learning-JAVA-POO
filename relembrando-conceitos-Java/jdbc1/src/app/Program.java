d34c053197e4897e5e46c266f07bb84178922f86
Fabio Matos <fabio.thunder@hotmail.com>
2022-04-05 18:44:08 -0300

    minha primeira conexao com banco de dados

---
+++ relembrando-conceitos-Java/jdbc1/src/app/Program.java
@@ -0,0 +1,18 @@
+package app;
+
+import db.DB;
+
+import java.sql.Connection;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+
+
+    }
+
+
+}
