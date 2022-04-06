ed9990c43b927f8331498a730630dde9cf2339b2
Fabio Matos <fabio.thunder@hotmail.com>
2022-04-06 11:36:41 -0300

    minha primeira atualização no banco de dados

--- relembrando-conceitos-Java/jdbc1/src/app/Program.java
+++ relembrando-conceitos-Java/jdbc1/src/app/Program.java
@@ -2,68 +2,42 @@ package app;
 
 import db.DB;
 
-import java.sql.*;
-import java.text.SimpleDateFormat;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
         Connection conn = null;
         PreparedStatement st = null;
 
         try {
+
             conn = DB.getConnection();
 
-            /*
             st = conn.prepareStatement(
+                    "UPDATE seller " +
+                            "SET BaseSalary = BaseSalary + ? " +
+                            "WHERE " +
+                            "(DepartmentId = ?)");
 
-                "INSERT INTO seller " +
-                        "(Name, Email, Birthdate, BaseSalary, DepartmentId) " +
-                        "VALUES" +
-                        "(?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
+            st.setDouble(1, 200.00);
+            st.setInt(2, 2);
 
-                st.setString(1, "Carl Purple");
-                st.setString(2, "carl@gmail.com");
-                st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-                st.setDouble(4, 3000.00);
-                st.setInt(5, 4);
+            int rowsAffected = st.executeUpdate();
 
-             */
-
-            st = conn.prepareStatement(
-                    "insert into department (Name) values ('D1'), ('D2')",
-                        Statement.RETURN_GENERATED_KEYS);
-
-                int rowsAffected = st.executeUpdate();
-
-                if(rowsAffected > 0) {
-                    ResultSet rs = st.getGeneratedKeys();
-                    while (rs.next()) {
-                        int id = rs.getInt(1);
-                        System.out.println("Done! ID = " + id);
-                    }
-                }
-                else {
-                    System.out.println("No rown affected!");
-                }
-
+            System.out.println("DONE! Rows Affected: " + rowsAffected);
 
         }
         catch(SQLException e) {
             e.printStackTrace();
         }
-//        catch(ParseException e) {
-//            e.printStackTrace();
-//        }
         finally {
             DB.closeStatement(st);
             DB.closeConnection();
         }
-
 
     }
 
