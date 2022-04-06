b2938fe4d7454de78a95b2994cd61b0231574b8f
Fabio Matos <fabio.thunder@hotmail.com>
2022-04-06 13:21:18 -0300

    aprendendo transações com jdbc

--- relembrando-conceitos-Java/jdbc1/src/app/Program.java
+++ relembrando-conceitos-Java/jdbc1/src/app/Program.java
@@ -1,37 +1,48 @@
 package app;
 
 import db.DB;
-import db.DBIntegrityException;
+import db.DbException;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Connection conn = null;
-        PreparedStatement st = null;
+        Statement st = null;
 
         try {
 
             conn = DB.getConnection();
 
-            st = conn.prepareStatement(
-                    "DELETE FROM department " +
-                            "WHERE " +
-                            "Id = ?");
+            conn.setAutoCommit(false);
 
-            st.setInt(1,2);
+            st = conn.createStatement();
 
-            int rowsAffected = st.executeUpdate();
+            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 
-            System.out.println("DONE! Rows Affected: " + rowsAffected);
+//            int x = 1;
+//            if(x < 2) {
+//                throw new SQLException("Fake error");
+//            }
 
+            int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+            conn.commit();
+
+            System.out.println("Rows1 " + rows1);
+            System.out.println("Rows2 " + rows2);
         }
         catch(SQLException e) {
-            throw new DBIntegrityException(e.getMessage());
+            try {
+                conn.rollback();
+                throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+            } catch (SQLException ex) {
+                throw new DbException("Error trying to rollback! Caused by: " + ex.getMessage());
+            }
         }
         finally {
             DB.closeStatement(st);
